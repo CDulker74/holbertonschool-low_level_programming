2acc7237744324eacbfd544326543dd8f331d34f
CDulker74 <9775@golbertonstudents.com>
2024-10-23 17:33:20 +0000

    7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/**                                                                                                                                                                                                                                                                     
- * main - Prints the lowercase alphabet in reverse,                                                                                                                                                                                                                     
- *        followed by a new line                                                                                                                                                                                                                                        
- *                                                                                                                                                                                                                                                                      
- * Return: Always 0 (Success)                                                                                                                                                                                                                                           
+/**
+ * main - Prints the lowercase alphabet in reverse,
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
