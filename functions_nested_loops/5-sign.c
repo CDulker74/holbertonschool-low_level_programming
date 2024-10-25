728295a8e6744c6e172fa05f6a069f4f10f45663
CDulker74 <9775@holbertonstudents.com>
2024-10-25 11:27:09 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-c
 #include "main.h"
 
 /**
@@ -11,19 +10,4 @@ c
  */
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else
-{
-_putchar('-');
-return (-1);
-}
-}
+    //
