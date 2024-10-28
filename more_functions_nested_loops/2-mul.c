8151df3576d590206142302866daf75540bb8ad2
CDulker74 <9775@holbertonstudents.com>
2024-10-28 11:22:19 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - multiple deux entiers
+* @a: premier entier
+* @b: deuxieme entier
+* Return: le resultat de la multiplication
+*/
+int mul (int a, int b)
+{
+return (a * b);
+}
