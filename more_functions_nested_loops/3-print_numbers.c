55ea4c5052853e48011ade50f50e5368408824b0
CDulker74 <9775@holbertonstudents.com>
2024-10-28 14:10:47 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
+/**
+*print_numbers - affiche les chiffres de 0 a 9 suivis d'une nouvelles lignes
+*/
+void print_numbers(void)
 {
 int i;
 
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 9; i++)
 {
 _putchar(i + '0');
 }
-_putchar ('\n');
+_putchar('\n');
 }
- 
-   
