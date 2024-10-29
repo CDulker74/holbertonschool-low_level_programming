5c2727a2401bf363b62dd25d22098fb1695edb89
CDulker74 <9775@holbertonstudents.com>
2024-10-29 14:38:23 +0000

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - convertit une chaîne en entier
+ * @s: chaîne à convertir
+ *
+ * Return: l'entier converti
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > INT_MAX / 10 ||
+		    (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+		{
+			return (sign == 1 ? INT_MAX : INT_MIN);
+		}
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return result * sign;
+}
