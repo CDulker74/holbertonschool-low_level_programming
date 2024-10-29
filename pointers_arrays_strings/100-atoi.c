31d905a39b07f58fbd165d47648f5e6d0b425609
CDulker74 <9775@holbertonstudents.com>
2024-10-29 14:46:21 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,31 +9,31 @@
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-    /* Ignorer les caractères non numériques au début */
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-            break; /* Sortir de la boucle dès qu'un chiffre est trouvé */
-        i++;
-    }
+	/* Ignorer les caractères non numériques au début */
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break; /* Sortir de la boucle dès qu'un chiffre est trouvé */
+		i++;
+	}
 
-    /* Convertir les chiffres en entier */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        /* Vérifier le débordement */
-        if (result > (INT_MAX - (s[i] - '0')) / 10)
-        {
-            return (sign == 1 ? INT_MAX : INT_MIN);
-        }
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+	/* Convertir les chiffres en entier */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		/* Vérifier le débordement */
+		if (result > (INT_MAX - (s[i] - '0')) / 10)
+		{
+			return (sign == 1 ? INT_MAX : INT_MIN);
+		}
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-    return result * sign;
+	return (result * sign);
 }
