d95f152dc1cf026f95b7f9b36064e531e7d0f6cd
CDulker74 <9775@holbertonstudents.com>
2024-10-29 13:04:47 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * _puts - imprime une chaine suivie d'un saut de ligne 
+ * _puts - imprime une chaine suivie d'un saut de ligne
  * @str: la chaine a imprimer
  */
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-
-		i++;
+		_putchar(*str);
+		str++;
 	}
-
 	_putchar('\n');
 }
