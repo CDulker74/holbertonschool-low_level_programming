10a8ff4293253a67892a3fa1502362fff49e30fa
CDulker74 <9775@holbertonstudents.com>
2024-10-29 13:50:59 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+void puts2(char *str)
+{
+	input_string[ ::2]
+}
+
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar void puts2(char *str)
+		}
+		i++;
+	}
+
+	_putchar ('\n');
+}
