ec6caa780bb5824e6bc7cf80eeb3e332d52bf6b6
CDulker74 <9775@holbertonstudents.com>
2024-10-29 14:39:46 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>  // Inclure limits.h pour INT_MAX et INT_MIN
 
 /**
  * _atoi - convertit une chaîne en entier
@@ -8,27 +9,30 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+    int result = 0;
+    int sign = 1;
+    int i = 0;
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
-	}
+    // Ignorer les caractères non numériques au début
+    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (result > INT_MAX / 10 ||
-		    (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-		{
-			return (sign == 1 ? INT_MAX : INT_MIN);
-		}
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+    // Convertir les chiffres en entier
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        // Vérifier le débordement
+        if (result > INT_MAX / 10 ||
+            (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+        {
+            return (sign == 1 ? INT_MAX : INT_MIN);
+        }
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
 
-	return result * sign;
+    return result * sign;
 }
