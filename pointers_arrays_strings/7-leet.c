b09ba8aa2290d04c0677c324ed13ff9ecbf32b75
CDulker74 <9775@holbertonstudents.com>
2024-10-30 22:15:55 +0000

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,3 +1,21 @@
+/**
+ * leet - Encodes a string into 1337
+ * @s: The string to be encoded
+ *
+ * Description: This function replaces certain letters with numbers:
+ * 'a' and 'A' are replaced by '4'
+ * 'e' and 'E' are replaced by '3'
+ * 'o' and 'O' are replaced by '0'
+ * 't' and 'T' are replaced by '7'
+ * 'l' and 'L' are replaced by '1'
+ *
+ * Return: A pointer to the encoded string
+ */
+char *leet(char *s)
+{
+    // Le reste de votre code ici
+}
+
 char *leet(char *s)
 {
 	int i, j;
