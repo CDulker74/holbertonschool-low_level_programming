4530c5e02e2fb43b8d24f33311a4522b5536bdc9
CDulker74 <9775@holbertonstudents.com>
2024-10-30 21:55:41 +0000

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncpy - copie n caractères de src vers dest
+ * _strncpy - copie une chaîne
  * @dest: chaîne de destination
  * @src: chaîne source
  * @n: nombre de caractères à copier
@@ -12,11 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	/* Copier les caractères de src vers dest */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	/* Si n est plus grand que la longueur de src, remplir le reste avec des '\0' */
 	for (; i < n; i++)
 		dest[i] = '\0';
 
