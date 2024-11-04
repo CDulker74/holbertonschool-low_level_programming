7eef58357225b233f810fd15611376ead3db25d7
CDulker74 <9775@holbertonstudents.com>
2024-11-04 14:41:18 +0000

    2-strchr.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memcpy - Copie une zone mémoir
+* @dest: Pointeur vers la zone mémoire de destinatio
+* @src: Pointeur vers la zone mémoire sourc
+* @n: Nombre d'octets a copier
+*
+* Return: Un pointeur vers la zone mémoire des
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+}
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strchr - Localise un caractere dans une chaine
+* @s : La chaine a parcourir
+* @c : le caractere a trouver
+*
+* Return: un pointeur vers la premiere occurence du caractere c dans la
+* chaine s, ou NULL si le caractere n'est pas trouve
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+s++;
+}
+
+if (*s == c)
+return (s);
+
+return (NULL);
+}
