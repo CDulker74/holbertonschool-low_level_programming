276d5a70e4b443e408cb2d8cf40da5d207648736
CDulker74 <9775@holbertonstudents.com>
2024-11-05 10:02:08 +0000

    8-main.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - Affiche la somme des deux diagonales
- * d'une matrice carrée d'entiers
- * @a: Pointeur vers le premier élément de la matri
+ * d'une matrice carrée d'entier
+ * @a: Pointeur vers le premier élément de la matce
  * @size: Taille de la matrice (nombre de lignes ou de colonnes)
  */
 void print_diagsums(int *a, int size)
