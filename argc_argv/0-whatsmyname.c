1f54272135fd7d336ac6e79c52fee3b43b957ab5
CDulker74 <9775@holbertonstudents.com>
2024-11-08 20:17:38 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    (void)argc; /* Pour éviter l'avertissement de paramètre non utilisé */
-    printf("%s\n", argv[0]);
-    return (0);
+(void)argc; /* Pour éviter l'avertissement de paramètre non utilisé */
+printf("%s\n", argv[0]);
+return (0);
 }
