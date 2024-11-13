d98ed8d665198c3ccac80519fc2fa6a2ea1242b2
CDulker74 <9775@holbertonstudents.com>
2024-11-13 16:13:39 +0000

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 
 /**
  * _strdup - Renvoie un pointeur vers un nouvel espace mémoire alloué,
- * qui contient une copie de la chaîne donnée en paramètre.
+ *           qui contient une copie de la chaîne donnée en paramètre.
  * @str: La chaîne à dupliquer
  *
  * Return: Un pointeur vers la chaîne dupliquée, ou NULL en cas d'échec
@@ -15,14 +15,17 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
+	/* Calcule la longueur de la chaîne d'entrée */
 	for (len = 0; str[len]; len++)
-		
-		
+		;
+
+	/* Alloue de la mémoire pour la chaîne dupliquée */
 	dup = malloc(sizeof(char) * (len + 1));
 
 	if (dup == NULL)
 		return (NULL);
 
+	/* Copie la chaîne */
 	for (i = 0; i <= len; i++)
 		dup[i] = str[i];
 
