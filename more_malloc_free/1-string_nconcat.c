bcc2279f3ff0883f8dfaf8797b4056f693788bbf
CDulker74 <9775@holbertonstudents.com>
2024-11-15 19:39:26 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,49 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatène deux chaine
- * @s1: premiere chaine 
- * @s2: deuxieme chaine 
- * @n: nombre de bytes de s2 a concaténer
+ * string_nconcat - Concatène deux chaînes de caractères
+ * @s1: La première chaîne
+ * @s2: La deuxième chaîne
+ * @n: Le nombre d'octets à prendre de s2
  *
- *  Retrun: pointeur vers la nouvelle chaine, ou NULL is echec
+ * Return: Pointeur vers un nouvel espace mémoire, ou NULL en cas d'échec
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len1 = 0, len2 = 0, i, j;
+    char *concat;
+    unsigned int len1 = 0, len2 = 0, i, j;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+    /* Traiter NULL comme une chaîne vide */
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
+    /* Calculer les longueurs de s1 et s2 */
+    while (s1[len1])
+        len1++;
+    while (s2[len2])
+        len2++;
 
-	if (n >= len2)
-		n = len2;
+    /* Utiliser toute s2 si n est supérieur ou égal à sa longueur */
+    if (n >= len2)
+        n = len2;
 
-	concat = malloc(sizeof(char) * (len1 + n + 1));
-	if (concat == NULL)
-		return (NULL);
+    /* Allouer de la mémoire pour la chaîne concaténée */
+    concat = malloc(sizeof(char) * (len1 + n + 1));
+    if (concat == NULL)
+        return (NULL);
 
-	for (i = 0; i < len1; i++)
-		concat[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
-		concat[i] = s2[j];
-	concat[i] = '\0';
+    /* Copier s1 dans concat */
+    for (i = 0; i < len1; i++)
+        concat[i] = s1[i];
 
-	return (concat);
+    /* Copier n octets de s2 dans concat */
+    for (j = 0; j < n; j++, i++)
+        concat[i] = s2[j];
+
+    /* Terminer la chaîne par un caractère nul */
+    concat[i] = '\0';
+
+    return (concat);
 }
