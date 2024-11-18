ce061f209a48ba428227015d28492ab3502ef45c
CDulker74 <9775@holbertonstudents.com>
2024-11-18 20:15:21 +0000

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - imprime un nom en utilisant une fonction donnée
+ * @name: nom à imprimer
+ * @f: pointeur de fonction vers la fonction d'impression
+ *
+ * Description: Cette fonction prend un nom et un pointeur de fonction
+ * comme arguments. Elle appelle la fonction pointée par f avec le nom
+ * comme argument, si les deux paramètres sont non NULL.
+ */
+void print_name(char *name, void (*f)(char *))
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
