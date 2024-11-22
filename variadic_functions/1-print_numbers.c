474564c48644f89ca8e3c78552675aacbcbc839c
CDulker74 <9775@holbertonstudents.com>
2024-11-22 21:43:23 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,29 +3,30 @@
 #include <stdarg.h>
 
 /**
- * print_numbers - Imprime des nombres, suivis d'une nouvelle ligne
+ * print_numbers - Imprime des nombres suivis d'une nouvelle ligne
  * @separator: La chaîne à imprimer entre les nombres
  * @n: Le nombre d'entiers passés à la fonction
  *
- * Return: void
+ * Description: Cette fonction imprime les nombres passés en arguments,
+ * séparés par la chaîne 'separator' si elle n'est pas NULL.
+ * Une nouvelle ligne est imprimée à la fin.
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
+	va_list args;
+	unsigned int i;
 
-    va_start(args, n); // Initialisation de la liste d'arguments
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(args, int)); // Imprime le nombre actuel
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
 
-        // Imprime le séparateur si ce n'est pas le dernier nombre et si le séparateur n'est pas NULL
-        if (i < n - 1 && separator != NULL)
-            printf("%s", separator);
-    }
+	printf("\n");
 
-    printf("\n"); // Nouvelle ligne à la fin
-
-    va_end(args); // Nettoyage de la liste d'arguments
+	va_end(args);
 }
