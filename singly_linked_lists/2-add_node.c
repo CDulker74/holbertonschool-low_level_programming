71d2aa587addc6583e8b27dc0121c05d5ed15831
CDulker74 <9775@holbertonstudents.com>
2024-12-04 19:53:03 +0100

    gcc 2-add_none.c finish

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,24 +2,23 @@
 #include <string.h>
 #include "lists.h"
 
-/
- 
-add_node - ajoute un nouveau nœud au début d'une liste list_t
-@head: pointeur vers le pointeur de tête de la liste
-@str: chaîne à dupliquer dans le nouveau nœud*
-Return: l'adresse du nouvel élément, ou NULL si échec*/
-list_t *add_node(list_t head, const char str)
+/**
+ * add_node - ajoute un nouveau nœud au début d'une liste list_t
+ * @head: pointeur vers le pointeur de la tête de la liste
+ * @str: chaîne à dupliquer dans le nouveau nœud
+ *
+ * Return: adresse du nouvel élément, ou NULL si échec
+ */
+list_t *add_node(list_t **head, const char *str)
 {
-    list_tnew_node;
-    char dup_str;
-    unsigned int len = 0;
+    list_t *new_node;
+    char *dup_str;
+    int len;
 
-    / Créer un nouveau nœud /
     new_node = malloc(sizeof(list_t));
     if (new_node == NULL)
         return (NULL);
 
-    / Dupliquer la chaîne /
     dup_str = strdup(str);
     if (dup_str == NULL)
     {
@@ -27,16 +26,12 @@ list_t *add_node(list_t head, const char str)
         return (NULL);
     }
 
-    / Calculer la longueur de la chaîne /
-    while (str[len])
+    for (len = 0; str[len];)
         len++;
 
-    / Initialiser le nouveau nœud /
     new_node->str = dup_str;
     new_node->len = len;
-    new_node->next =head;
-
-    /* Faire pointer la tête vers le nouveau nœud */
+    new_node->next = *head;
     *head = new_node;
 
     return (new_node);
