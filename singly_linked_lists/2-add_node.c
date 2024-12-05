6b9663e2d7f83f5614c4abbf14e4a933fac6b48f
CDulker74 <9775@holbertonstudents.com>
2024-12-05 19:40:35 +0100

    fixing file 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,28 +11,28 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *dup_str;
-    int len;
+list_t *new_node;
+char *dup_str;
+int len;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
 
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+dup_str = strdup(str);
+if (dup_str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-    for (len = 0; str[len];)
-        len++;
+for (len = 0; str[len];)
+len++;
 
-    new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
+new_node->str = dup_str;
+new_node->len = len;
+new_node->next = *head;
+*head = new_node;
 
-    return (new_node);
+return (new_node);
 }
