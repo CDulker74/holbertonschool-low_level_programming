8ba8a9e30160259cc434104b73a4b6487525ac1e
CDulker74 <9775@holbertonstudents.com>
2024-12-05 19:43:27 +0100

    fixing file 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,39 +11,38 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *last;
-    char *dup_str;
-    int len;
+list_t *new_node, *last;
+char *dup_str;
+int len;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
 
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+dup_str = strdup(str);
+if (dup_str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+for (len = 0; str[len];)
+len++;
 
-    for (len = 0; str[len];)
-        len++;
+new_node->str = dup_str;
+new_node->len = len;
+new_node->next = NULL;
 
-    new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = NULL;
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+last = *head;
+while (last->next != NULL)
+last = last->next;
 
-    last = *head;
-    while (last->next != NULL)
-        last = last->next;
+last->next = new_node;
 
-    last->next = new_node;
-
-    return (new_node);
+return (new_node);
 }
