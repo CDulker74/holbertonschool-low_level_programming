3a4204b6570eaf6012708f4121afe2d82c6d326f
CDulker74 <9775@holbertonstudents.com>
2024-12-06 22:33:53 +0100

    fixing error maybe file 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * read_textfile - Read a text file and print to POSIX stdout
- * @filename: Name of the file to read
- * @letters: Number of letters to read and print
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: Actual number of letters read and printed
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-char *buffer;
-ssize_t fd, bytes_read, bytes_written;
+int fd;
+ssize_t nread, nwritten;
+char *buf;
 
 if (!filename)
 return (0);
@@ -20,28 +20,28 @@ fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (!buffer)
+buf = malloc(sizeof(char) * letters);
+if (!buf)
 {
 close(fd);
 return (0);
 }
 
-bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
+nread = read(fd, buf, letters);
+if (nread == -1)
 {
-free(buffer);
+free(buf);
 close(fd);
 return (0);
 }
 
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+nwritten = write(STDOUT_FILENO, buf, nread);
 
-free(buffer);
+free(buf);
 close(fd);
 
-if (bytes_written != bytes_read)
+if (nwritten != nread)
 return (0);
 
-return (bytes_written);
+return (nwritten);
 }
