3ddd4e2a77a255cc2de03c1bd5785aa933cf586d
CDulker74 <9775@holbertonstudents.com>
2024-12-06 15:00:10 +0100

    full fixing betty coding style the files on my directory

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - Retourne la somme de toutes les données (n) d'une liste dlistint_t
+ * sum_dlistint - Somme toutes les données (n) d'une liste dlistint_t
  * @head: Pointeur vers la tête de la liste
  *
- * Return: La somme de toutes les données, ou 0 si la liste est vide
+ * Return: La somme des données, ou 0 si la liste est vide
  */
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+int sum = 0;
 
-    while (head != NULL)
-    {
-        sum += head->n;
-        head = head->next;
-    }
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
 
-    return sum;
+return (sum);
 }
--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Supprime le nœud à l'index donné d'une liste dlistint_t
+ * delete_dnodeint_at_index - Supprime le nœud à l'index donné
  * @head: Pointeur vers un pointeur sur la tête de la liste
  * @index: Index du nœud à supprimer
  *
@@ -10,37 +10,37 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp = *head;
-    unsigned int i = 0;
+dlistint_t *temp = *head;
+unsigned int i = 0;
 
-    if (temp == NULL)
-        return (-1);
+if (temp == NULL)
+return (-1);
 
-    if (index == 0)
-    {
-        *head = temp->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(temp);
-        return (1);
-    }
+if (index == 0)
+{
+*head = temp->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+free(temp);
+return (1);
+}
 
-    while (temp != NULL && i < index)
-    {
-        temp = temp->next;
-        i++;
-    }
+while (temp != NULL && i < index)
+{
+temp = temp->next;
+i++
+}
 
-    if (temp == NULL)
-        return (-1);
+if (temp == NULL)
+return (-1);
 
-    if (temp->prev != NULL)
-        temp->prev->next = temp->next;
+if (temp->prev != NULL)
+temp->prev->next = temp->next;
 
-    if (temp->next != NULL)
-        temp->next->prev = temp->prev;
+if (temp->next != NULL)
+temp->next->prev = temp->prev;
 
-    free(temp);
+free(temp);
 
-    return (1);
+return (1);
 }
