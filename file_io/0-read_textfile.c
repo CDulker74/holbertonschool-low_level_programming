f3d213e1b624cff54306c358ddb77dc8524002f5
CDulker74 <9775@holbertonstudents.com>
2024-12-06 22:29:01 +0100

    maybe finish file 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
-* read_textfile - li un fichier texte et l'imprime sur la sortie standard POSIX
- * @filename: le nom du fichier à lire
- * @letters: le nombre de lettres à lire et imprimer
+ * read_textfile - Read a text file and print to POSIX stdout
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
  *
- * Return: le nombre réel de lettres lues et imprimées, 0 si erreur
+ * Return: Actual number of letters read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t nread, nwritten;
-char *buffer;
 
-if (filename == NULL)
+char *buffer;
+ssize_t fd, bytes_read, bytes_written;
+
+if (!filename)
 return (0);
 
 fd = open(filename, O_RDONLY);
@@ -21,27 +21,27 @@ if (fd == -1)
 return (0);
 
 buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
+if (!buffer)
 {
 close(fd);
 return (0);
 }
 
-nread = read(fd, buffer, letters);
-if (nread == -1)
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
 {
 free(buffer);
 close(fd);
 return (0);
 }
 
-nwritten = write(STDOUT_FILENO, buffer, nread);
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 free(buffer);
 close(fd);
 
-if (nwritten != nread)
+if (bytes_written != bytes_read)
 return (0);
 
-return (nwritten);
+return (bytes_written);
 }
