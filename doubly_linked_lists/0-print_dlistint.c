f925362f4948c2b6d0241d937b8df06629c2cce8
CDulker74 <9775@holbertonstudents.com>
2024-12-06 14:46:08 +0100

    fixing file 0 betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,21 +2,21 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Imprime tous les éléments d'une liste dlistint_t
- * @h: Pointeur vers la tête de la liste
- *
- * Return: Le nombre de nœuds
- */
+* print_dlistint - Imprime tous les éléments d'une liste dlistint_t
+* @h: Pointeur vers la tête de la liste
+*
+* Return: Le nombre de nœuds
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
 
-    return count;
+return (count);
 }
