c2809decedeadb736c9810cc9a5b3e3b9a23f2b4
CDulker74 <9775@holbertonstudents.com>
2024-12-06 22:45:35 +0100

    finish maybe file 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,25 +10,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, w_status;
-    
-    if (!filename)
-        return (-1);
-    
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
-    
-    if (text_content)
-    {
-        w_status = write(fd, text_content, strlen(text_content));
-        if (w_status == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-    
-    close(fd);
-    return (1);
+int fd, w_status;
+
+if (!filename)
+return (-1);
+
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
+
+if (text_content)
+{
+w_status = write(fd, text_content, strlen(text_content));
+if (w_status == -1)
+{
+close(fd);
+return (-1);
+}
+}
+
+close(fd);
+return (1);
 }
