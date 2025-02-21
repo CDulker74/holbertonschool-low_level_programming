075a3108ffd4151aa0c4e32e5ae643160ad38403
CDulker74 <9775@holbertonstudents.com>
2025-02-21 17:14:21 +0100

    finish file 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of jack bauer
+ */
+void jack_bauer(void)
+{
+int hour, minute
+
+for (hour = 0; hour < 24; hour++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');          
+_putchar('\n');
+}
+}
