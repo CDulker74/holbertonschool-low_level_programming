ada790f283f813d177e54a5ac503a3a5849e95a2
CDulker74 <9775@holbertonstudents.com>
2025-02-21 16:44:48 +0100

    finish file 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,20 @@
 
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times in lowercase
- * 
+ *
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-    int i, j;
-    char c;
+int i, j;
+char c;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0, c = 'a'; j < 26; j++, c++)
-        {
-            _putchar(c);
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < 10; i++)
+{
+for (j = 0, c = 'a'; j < 26; j++, c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
 }
