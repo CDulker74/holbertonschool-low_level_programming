e6dea9e2813ed97570c06e72f1e48adf9541827e
CDulker74 <9775@holbertonstudents.com>
2025-02-26 23:17:11 +0100

    finish file 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,15 @@
  */
 void more_numbers(void)
 {
-int i, j, tens, ones;
+int i, j;
 
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-tens = j / 10;
-ones = j & 10;
-
 if (j > 9)
-_putchar(tens + '0');
-_putchar(ones + '0');           
-
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 }
 _putchar('\n');
 }
