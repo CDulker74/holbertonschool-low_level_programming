5044cccd12bbf5538f3556f2671c1f390de98bc0
CDulker74 <9775@holbertonstudents.com>
2025-02-27 21:46:02 +0100

    finish file 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ while (*s)
 if (*s == '-')
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
-    {
+{
 digit = *s - '0';
 if (result > (INT_MAX - digit) / 10)
 return (sign == 1 ? INT_MAX : INT_MIN);
