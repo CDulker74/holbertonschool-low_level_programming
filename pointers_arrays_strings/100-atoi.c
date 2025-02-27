0c7ff7bfa0e83463e1ffeb6a557c0c52a539e8db
CDulker74 <9775@holbertonstudents.com>
2025-02-27 21:45:21 +0100

    finish file 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,25 @@
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int digit;
+int result = 0;
+int sign = 1;
+int digit;
 
-    while (*s)
+while (*s)
+{
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
     {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-        {
-            digit = *s - '0';
-            if (result > (INT_MAX - digit) / 10)
-                return (sign == 1 ? INT_MAX : INT_MIN);
-            result = result * 10 + digit;
-        }
-        else if (result > 0)
-            break;
-        s++;
-    }
+digit = *s - '0';
+if (result > (INT_MAX - digit) / 10)
+return (sign == 1 ? INT_MAX : INT_MIN);
+result = result * 10 + digit;
+}
+else if (result > 0)
+break;
+s++;
+}
 
-    return (result * sign);
+return (result * sign);
 }
