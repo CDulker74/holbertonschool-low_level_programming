f04fee6a871994c20750607d49960191d82532c8
CDulker74 <9775@holbertonstudents.com>
2025-03-01 20:44:28 +0100

    finish file 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,7 +9,7 @@
  * Return: A pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
- {
+{
 int dest_len = 0;
 int i;
 
@@ -17,7 +17,10 @@ while (dest[dest_len] != '\0')
 dest_len++;
 
 for (i = 0; i < n && src[i] != '\0'; i++)
+dest[dest_len + i] = src[i];
+
+if (i < n)
 dest[dest_len + i] = '\0';
 
 return dest;
- }
+}
