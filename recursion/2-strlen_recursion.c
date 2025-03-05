74f9065869c611e2b9b8abffb13b1e773ff7b5fa
CDulker74 <9775@holbertonstudents.com>
2025-03-05 06:15:33 +0100

    finish file 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion - Returns the length of a string
- * @s: The string to mesure
+ * @s: The string to measure
  *
  * Return: The length of the string
  */
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return (0);
+return 0;
 }
-return ((1 + _strlen_recursions + 1;))
+return 1 + _strlen_recursion(s + 1);
 }
