5e18cf90cf17a4a2f5fe498f3b9b810fd70dfdb7
CDulker74 <9775@holbertonstudents.com>
2025-03-06 01:22:28 +0100

    finish file 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the program
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 (void)argc;
